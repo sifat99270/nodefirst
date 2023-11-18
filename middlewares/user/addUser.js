7d5bd5887e072fdce44e8fb75e3e3ee5eaf3e97f
smn <rasifat33@gmail.com>
2023-11-18 10:49:52 +0600

    o

--- middlewares/user/addUser.js
+++ middlewares/user/addUser.js
@@ -1,23 +1,25 @@
-const Hash = require("../../other/hash");
+//const Hash = require("../../other/hash");
 const User = require("../../models/usersSchema");
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require("bcrypt");
 //get user
 async function getUser(req, res, next) {
   let newUser;
-  const pass = req.body.password;
-  const hash = await Hash(pass);
+  const pass = req.body.password.toString();
+
+  const hashPass = await bcrypt.hash(pass, 10);
+  //const hash = await Hash(pass);
   if (req.files && req.files.length > 0) {
     newUser = new User({
       ...req.body,
       avatar: req.files[0].filename,
-      password: hash,
+      password: hashPass,
     });
   } else {
     newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      password: hash,
+      password: hashPass,
     });
   }
   try {
@@ -31,7 +33,6 @@ async function getUser(req, res, next) {
         avatar: find.avatar,
       };
       const token = jwt.sign(loginObj, process.env.JWT_SECRET);
-      console.log(token);
       return res.status(200).json({
         token,
         loginObj,
